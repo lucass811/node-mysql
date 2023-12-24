f18b829be1659cc19fd3af596a39ab203d830be1
Lucas <lucas.desenvolvedor@hotmail.com>
2023-12-24 14:21:34 -0300

    configuração do handlebars

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 
+const { engine } = require('express-handlebars')
+
+const mysql = require('mysql2');
+
 const app = express();
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+const con = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'admin',
+    database:'projeto2'
+
+});
+
+con.connect((erro) => {
+    if(erro) throw erro;
+    console.log('Conexão efetuada com sucesso');
+});
+
 app.get('/', (req, res) => {
-   res.write('Ola');
-   res.end();
+    res.render('formulario')
 });
 
 app.listen(3000);
